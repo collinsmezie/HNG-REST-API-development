bbe97a12d1647a4541165bfead0e34770c8a449e
collins <collinschimezieobinwa@gmail.com>
2023-09-15 13:42:24 +0100

    modify IP version

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ const app = express();
 
 app.use(express.json());
 
-// mongoose.connect("mongodb://localhost:27017/test", {
-//   useNewUrlParser: true,
-// });
-
-mongoose.connect("mongodb://127.0.0.1:27017/test", {
+mongoose.connect("mongodb://localhost:27017/test", {
   useNewUrlParser: true,
 });
+
+// mongoose.connect("mongodb://127.0.0.1:27017/test", {
+//   useNewUrlParser: true,
+// });
 
 
 const db = mongoose.connection;
